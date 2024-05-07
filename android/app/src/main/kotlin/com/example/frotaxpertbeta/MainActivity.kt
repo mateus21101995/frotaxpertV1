177a1a59c1af6638414e18b734e740cf5f66e089
Mateus Osvaldo <65655500+mateus21101995@users.noreply.github.com>
2024-05-07 14:57:23 +0100

    upload do aplicativo

---
+++ android/app/src/main/kotlin/com/example/frotaxpertbeta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frotaxpertbeta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(
+      Uri.parse('https://www.frotaxpert.ao/entrar'),
+    );
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WebViewWidget(controller: controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
